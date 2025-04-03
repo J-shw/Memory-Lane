1cb210f039a08d041f51148206212f25d2a21ac8
Josh <91017208+J-shw@users.noreply.github.com>
2025-04-03 11:43:32 +0100

    Update main.py

---
+++ analysis/app/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models import File, Statistics, FileCreate, FileOut, SessionLocal
+import modules.process as process
+from datetime import datetime, timezone
+import uvicorn
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/files/", response_model=FileOut)
+def create_file(item: FileCreate, db: Session = Depends(get_db)):
+    db_item = File(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.get("/files/", response_model=list[FileOut])
+def read_files(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = db.query(File).offset(skip).limit(limit).all()
+    return items
+
+@app.get("/files/{event_id}", response_model=FileOut)
+def read_file(event_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(File).filter(File.id == event_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="File not found")
+    return db_item
+
+@app.get("/")
+async def root():
+    return {"message": "Analysis - System"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9090)
