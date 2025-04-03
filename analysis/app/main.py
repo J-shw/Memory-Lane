9a6e09e04fe8ee9e66a5cbf7624d108473c5d1e5
Josh <91017208+J-shw@users.noreply.github.com>
2025-04-03 12:27:24 +0100

    fixed directory

--- analysis/app/main.py
+++ analysis/app/main.py
@@ -36,7 +36,7 @@ def read_file(event_id: int, db: Session = Depends(get_db)):
 
 @app.get("/process")
 def process_files():
-    process_directory('/files')
+    process_directory('/app/files')
     return {"message": "Processing.."}
 
 @app.get("/")
