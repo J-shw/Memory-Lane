6f7a89a533cfb286fdb63ac92a4298740431f02b
Josh <91017208+J-shw@users.noreply.github.com>
2025-04-03 12:01:42 +0100

    Update process.py

---
+++ analysis/app/modules/process.py
@@ -0,0 +1,41 @@
+import os, datetime, requests, magic, logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+API_ENDPOINT = "http://localhost:9090/files/"
+
+def get_file_metadata(file_path):
+    try:
+        stat = os.stat(file_path)
+        name = os.path.basename(file_path)
+        size = stat.st_size
+        date_created = datetime.datetime.fromtimestamp(stat.st_birthtime)
+        #date_modified = datetime.datetime.fromtimestamp(stat.st_mtime)
+        mime = magic.Magic(mime=True)
+        mime_type = mime.from_file(file_path)
+        extension = os.path.splitext(file_path)[1][1:] #Get extension without the .
+        return {
+            "name": name,
+            "path": file_path,
+            "size": size,
+            "dateCreated": date_created,
+            "mimeType": mime_type,
+            "extension": extension,
+        }
+    except Exception as e:
+        logging.error(f"Error getting metadata for {file_path}: {e}")
+        return None
+
+def process_directory(directory):
+    for root, _, files in os.walk(directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            metadata = get_file_metadata(file_path)
+            if metadata:
+                try:
+                    response = requests.post(API_ENDPOINT, json=metadata)
+                    response.raise_for_status() 
+                    logging.info(f"File {file_path} added successfully.")
+                except requests.exceptions.RequestException as e:
+                    logging.error(f"Error adding file {file_path}: {e}")
