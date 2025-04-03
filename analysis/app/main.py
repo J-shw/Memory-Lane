9e342a9f76cbeb148c2ca5ca56d76a5c24a5d176
Josh <91017208+J-shw@users.noreply.github.com>
2025-04-03 16:59:49 +0100

    Added new routes for volumes + small renames

--- analysis/app/main.py
+++ analysis/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from models import File, Statistics, FileCreate, FileOut, SessionLocal
+from models import SessionLocal, File, FileCreate, FileOut, Volume, VolumeCreate, VolumeOut
 from modules.process import process_directory
 from datetime import datetime, timezone
 import uvicorn
@@ -27,11 +27,31 @@ def read_files(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = db.query(File).offset(skip).limit(limit).all()
     return items
 
-@app.get("/files/{event_id}", response_model=FileOut)
-def read_file(event_id: int, db: Session = Depends(get_db)):
-    db_item = db.query(File).filter(File.id == event_id).first()
+@app.get("/files/{id}", response_model=FileOut)
+def read_file(id: int, db: Session = Depends(get_db)):
+    db_item = db.query(File).filter(File.id == id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="File not found")
+    return db_item
+
+@app.post("/volumes/", response_model=VolumeOut)
+def create_volume(item: FileCreate, db: Session = Depends(get_db)):
+    db_item = Volume(**item.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.get("/volumes/", response_model=list[VolumeOut])
+def read_volumes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = db.query(Volume).offset(skip).limit(limit).all()
+    return items
+
+@app.get("/volumes/{id}", response_model=VolumeOut)
+def read_volume(id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Volume).filter(Volume.id == id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Volume not found")
     return db_item
 
 @app.get("/process")
